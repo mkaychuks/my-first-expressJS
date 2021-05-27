6a7eea0612e9d6ac5fa8a84b77607daae6e4abbe
mkaychuks <sachufusi@gmail.com>
2021-05-27 13:44:03 -0700

    working with mongoDB and Atlas

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 const express = require('express')
-
+const mongoose = require('mongoose')
 const blogRouters = require('./routes/blogRoutes')
+require('dotenv/config')
 
 const app = express() // init express
 
 // setting up middlewares
 app.use(express.json())
+
+// connecting to mongodb using mongoose
+mongoose
+  .connect(process.env.DB_CONNECTION, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  })
+  .then((result) => console.log('Connected to DB'))
+  .catch((err) => console.log(err))
 
 // serving routers as middlewares
 app.use('/api/v1/', blogRouters)
