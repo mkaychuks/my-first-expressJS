4b82ef86261d3ff58ec54119c2b5c3767560ff83
mkaychuks <sachufusi@gmail.com>
2021-05-28 14:43:14 -0700

    :rocket:

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -4,7 +4,7 @@ const Blog = require('../models/blog') // importing the blog model
 const blogHome = (req, res) => {
   Blog.find()
     .then((result) => {
-      res.status(201).json(result)
+      res.status(200).json({ success: true, data: result })
     })
     .catch((err) => console.log(err))
 }
