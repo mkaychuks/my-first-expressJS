5db0e92ae5e98318c4d2847aeae0be2768697e07
mkaychuks <sachufusi@gmail.com>
2021-05-29 15:17:18 -0700

    :space:

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -11,6 +11,6 @@ const router = Router()
 router.get('/', blogHome) // handling home GET requests
 router.post('/blog', blogCreate) // handling home POST requests
 router.get('/:id', blogSingle) // handling home GET requests for single posts
-router.delete('/:id', blogDelete)
+router.delete('/:id', blogDelete) //handling home GET requests for single posts
 
 module.exports = router
