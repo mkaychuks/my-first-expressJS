db2b6747b5660d4df394b538f62f06274dfcef09
mkaychuks <sachufusi@gmail.com>
2021-05-29 11:48:25 -0700

    getting a single post using the _id :rocket:

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -23,8 +23,22 @@ const blogCreate = (req, res) => {
       res.status(201).json({ success: true, data: result })
     })
     .catch((err) => {
-      console.log(err)
+      console.log({ success: false, message: err })
     })
 }
 
-module.exports = { blogHome, blogCreate }
+// pulling a single blog post
+const blogSingle = (req, res) => {
+  Blog.findById(req.params.id)
+    .then((result) => {
+      res.status(200).json({ success: true, data: result })
+    })
+    .catch((err) => res.status(404).json({ success: false, message: err }))
+}
+
+// updating or patching up and existing blog post
+const blogUpdate = (req, res) => {
+  Blog.updateOne({ _id: req.params.id })
+}
+
+module.exports = { blogHome, blogCreate, blogSingle }
--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,9 +1,14 @@
 const { Router } = require('express')
-const { blogHome, blogCreate } = require('../controllers/blogController')
+const {
+  blogHome,
+  blogCreate,
+  blogSingle,
+} = require('../controllers/blogController')
 
 const router = Router()
 
 router.get('/', blogHome) // handling home GET requests
-router.post('/blog', blogCreate)
+router.post('/blog', blogCreate) // handling home POST requests
+router.get('/:id', blogSingle) // handling home GET requests for single posts
 
 module.exports = router
